2dcff94a579791cd656f222cfbdd1e9ae3f31036
Bartosz Pomiankiewicz <01159433@pw.edu.pl>
2026-09-24 00:25:11 +0200

    feat: Proper response status for repeated email, Completed Exercise D3

--- src/main/java/io/github/pomian99/medical_clinic/exception/PatientAlreadyExistsException.java
+++ src/main/java/io/github/pomian99/medical_clinic/exception/PatientAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package io.github.pomian99.medical_clinic.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class PatientAlreadyExistsException extends RuntimeException {
     public PatientAlreadyExistsException(String message) {
         super(message);
